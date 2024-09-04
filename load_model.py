ee1b6823432f0ea8850c8313abf4bf4088e7eb14
RajaAhsanZahoor <ahsanzahoor11123@gmail.com>
2024-09-04 13:51:30 +0000

    First

---
+++ load_model.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import joblib
+from sklearn.metrics import accuracy_score
+
+model = joblib.load('K-Nearest Neighbors.pkl')
+
+test_data = pd.read_csv('mobile_price_range_data.csv')
+X_test = test_data.iloc[:, :-1]
+y_test = test_data.iloc[:, -1]
+
+y_pred = model.predict(X_test)
+
+accuracy= accuracy_score(y_test, y_pred)
+
+with open('accuracy.txt', 'w') as file:
+    file.write(f'Accuracy: {accuracy}')
